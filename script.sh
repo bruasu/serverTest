8ca55b0721bc4c6df0da5d712f4670a3e4523c63
bruasu <bruasu@gmail.com>
2018-09-28 15:16:52 -0300

    modificado el scrip

--- script.sh
+++ script.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-clear;echo Hola mundo
+
+#Actualizamos el servidor
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install ubuntu-restricted-extras -y
+
+sudo apt-get install apache2 php7.0 libapache2-mod-php7.0 mysql-server php7.0-mysql php7.0-mcrypt
+
+sudo apt install python3.6
