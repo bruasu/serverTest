2888956df58b678b28628a85c709c9c355f26b10
bruasu <bruasu@gmail.com>
2018-10-16 12:48:44 -0300

    Actualizacion phpsysinfo

--- bruasu.sh
+++ bruasu.sh
@@ -4,9 +4,13 @@ echo Iniciando la Instalacion by Bruasu
 
 sleep 4s
 
-cd ~
-wget https://www.apachefriends.org/xampp-files/7.2.10/xampp-linux-x64-7.2.10-0-installer.run
-
-sudo chmod 755 xampp-linux-x64-7.2.10-0-installer.run
-sudo ./xampp-linux-x64-7.2.10-0-installer.run
-rm xampp-linux-x64-7.2.10-0-installer.run
+sudo apt install apache2 php7.2 libapache2-mod-php7.2 php7.2-xml
+cd /var/www/html/
+sudo wget -c https://netcologne.dl.sourceforge.net/project/phpsysinfo/phpsysinfo/3.2.10/phpsysinfo-3.2.10.tar.gz
+sudo tar xzvf phpsysinfo-3.2.10.tar.gz
+sudo mv phpsysinfo-3.2.10 phpsysinfo
+sudo rm phpsysinfo-3.2.10.tar.gz
+sudo cp /var/www/html/phpsysinfo/phpsysinfo.ini.new /var/www/html/phpsysinfo/phpsysinfo.ini
+sudo chown -R www-data.www.data phpsysinfo/
+sudo ufw allow http
+sudo ufw allow https
