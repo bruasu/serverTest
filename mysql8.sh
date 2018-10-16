fb31086916deacd42a7503713ab9fa3a550402e0
bruasu <bruasu@gmail.com>
2018-10-16 14:33:43 -0300

    actualizado

--- mysql8.sh
+++ mysql8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Instalar Mysql
-sudo wget http://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
+wget http://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.10-1_all.deb
 sudo apt-get update
 sudo apt-get install mysql-server
